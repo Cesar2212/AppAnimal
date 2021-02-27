966c9c243bd8f1aa379a605033e8515e1eeb45e1
César <cesar2212sc@gmail.com>
2021-02-27 17:13:01 -0500

    creación de package mvvm y librerías

---
+++ app/src/main/java/com/codecasc/appanimal/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.codecasc.appanimal.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codecasc.appanimal.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
